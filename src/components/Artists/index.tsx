c318d059bcaf7a5474c3fa3f52f54572d12c5432
Washington <wiss@cin.ufpe.br>
2021-07-16 19:35:48 -0300

    create Artist component

---
+++ src/components/Artists/index.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import Carousel from 'react-elastic-carousel';
+import perfil from '../../asserts/img/perfil.png';
+import arte1 from '../../asserts/img/arte1.png';
+import arte2 from '../../asserts/img/arte2.png';
+import arte3 from '../../asserts/img/arte3.png';
+import { Image } from "@chakra-ui/react";
+
+export function Artists() {
+
+  const renderItem = () => {
+    const names = [
+      'Maria Luisa',
+      'Gabriel Oliveira',
+      'Washington Igor',
+      'Maria Luisa',
+      'Gabriel Oliveira',
+      'Washington Igor',
+    ];
+    return names.map((item: string) => (
+      <div key={item} className={styles.carouselContainer}>
+        <div className={styles.perfilInfo}>
+          <Image
+            boxSize="80px"
+            objectFit="cover"
+            src={perfil}
+            alt="Perfil"
+          />
+          <article>
+            <section>{item}</section>
+            <section>{item}</section>
+            <section>{item}</section>
+            <section>{item}</section>
+          </article>
+        </div>
+        <div className={styles.images}>
+        <Image
+            boxSize="100px"
+            objectFit="cover"
+            src={arte1}
+            alt="Perfil"
+          />
+         <Image
+            boxSize="100px"
+            objectFit="cover"
+            src={arte2}
+            alt="Perfil"
+          />
+          <Image
+            boxSize="100px"
+            objectFit="cover"
+            src={arte3}
+            alt="Perfil"
+          />
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h2>Artistas Recomendados</h2>
+      </div>
+      <Carousel
+        className={styles.carousel} 
+        isRTL={false} 
+        disableArrowsOnEnd
+        pagination={false}
+        itemsToShow={3}
+        enableMouseSwipe={false}
+      >
+        {renderItem()}
+      </Carousel>
+    </div>
+  );
+}
+
+export default Artists;
